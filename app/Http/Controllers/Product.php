c62c0f1b178b7c4f4f31e70ee68aed55aed11f99
aster1649 <asuasu9360@gmail.com>
2022-04-18 19:53:12 +0100

    first commit

---
+++ app/Http/Controllers/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class Product extends Controller
+
+{
+    public function register(){
+        return view(product.register);
+        } 
+        public function store(Request $request)
+        {
+        $Product=new product();
+        $product->name=$request->name;
+        $product->unit=$request->unit;
+        $product->price=$request->price;
+        $product->quantity=$request->quantity;
+        $is_succes=$product::save();
+        if($is_secces)
+        echo "record saved successfully";
+        else
+        echo "something want wrong . try again";
+        }
+
+        
+        
+
+
+    //
+}
---
+++ database/migrations/2022_04_18_113744_product.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //schema:: ceate("product",function,Blueprint,$table()
+        //schema:: create("product";Bluprint $ model(),)$table id();  }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
